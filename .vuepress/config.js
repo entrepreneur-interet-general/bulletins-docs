841e9380f9efaeb277ddcdf17f1bce09d1c31432
Antoine Augusti <antoine.augusti@data.gouv.fr>
2019-04-25 15:39:43 +0200

    Add favicon

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -10,6 +10,7 @@ module.exports = {
   title: 'Bulletins',
   description: 'Jeu de données des affaires maritimes sur l\'assistance et le sauvetage en mer',
   head: [
+    ['link', { rel: 'icon', href: '/favicon.png' }],
     ['meta', { name: 'theme-color', content: '#0053b3' }]
   ],
   themeConfig: {
